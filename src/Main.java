d023f80fe644d4b6459a7c3672ba2374f53044d0
Wizqqr <baban1979molotov@gmail.com>
2024-07-24 15:56:17 +0600

    Исправил баги

--- src/Main.java
+++ src/Main.java
@@ -2,23 +2,19 @@ public class Main {
     public static void main(String[] args) {
         FamilyTree familyTree = new FamilyTree();
 
-        // Создание людей
         Person john = new Person("John", 40, "Male");
         Person mary = new Person("Mary", 38, "Female");
         Person paul = new Person("Paul", 18, "Male");
         Person anna = new Person("Anna", 16, "Female");
 
-        // Добавление людей в генеалогическое дерево
         familyTree.addPerson(john);
         familyTree.addPerson(mary);
 
-        // Добавление детей
         familyTree.addChild("John", paul);
         familyTree.addChild("John", anna);
         familyTree.addChild("Mary", paul);
         familyTree.addChild("Mary", anna);
 
-        // Проведение исследований
         System.out.println("Children of John:");
         for (Person child : familyTree.getChildren("John")) {
             System.out.println(child);
